7b905c47ab5021a9789ece65927734a7f676e18d
Eric M Sandoval <ericmsandoval@gmail.com>
2018-03-10 09:08:34 -0800

    Dockerizing proxy server

--- server.js
+++ server.js
@@ -1,9 +1,69 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 
-app.use(express.static(`${__dirname}/public`));
-app.use('/listings/:id', express.static(`${__dirname}/public`));
+app.use('/:id', express.static(`${__dirname}/public`));
+
+app.use('/reviews', (req, res) => {
+  axios.get(`http://127.0.0.1:3001${req.originalUrl}`)
+    .then(moduleServerRes => moduleServerRes.data)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send();
+    });
+});
+
+app.use('/overview', (req, res) => {
+  axios.get(`http://127.0.0.1:3002${req.originalUrl}`)
+    .then(moduleServerRes => moduleServerRes.data)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send();
+    });
+});
+
+app.use('/nearby', (req, res) => {
+  axios.get(`http://127.0.0.1:3003${req.originalUrl}`)
+    .then(moduleServerRes => moduleServerRes.data)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send();
+    });
+});
+
+app.use('/q-and-a', (req, res) => {
+  axios.get(`http://127.0.0.1:3004${req.originalUrl}`)
+    .then(moduleServerRes => moduleServerRes.data)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send();
+    });
+});
+
+app.use('/recommendations', (req, res) => {
+  axios.get(`http://127.0.0.1:3005${req.originalUrl}`)
+    .then(moduleServerRes => moduleServerRes.data)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send();
+    });
+});
 
 app.listen(3000, () => {
   console.log('Proxy Server running at http://localhost:3000');
